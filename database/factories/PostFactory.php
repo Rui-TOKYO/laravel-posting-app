b2b97af7b28aca340c962fc4007c967e386d7075
Rui-TOKYO <reityo_rui@yahoo.co.jp>
2025-01-19 02:40:27 +0900

    課題「Twitter風アプリに機能を追加しよう」を追加

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,9 +18,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => 2,
-            'title' => fake()->company(),
-            'content' => fake()->realText()
+            'user_id' => 2, // usersテーブルにidカラムの値が1のユーザーが存在することが前提
+            'title' => fake()->realText(20, 5),
+            'content' => fake()->realText(200, 5)
         ];
     }
 }
